b4dcdcffd33470152a6af21715a694dacfae75ec
adityakangokar <aditya.k@glocalmind.com>
2022-08-03 01:19:34 +0530

    login added

--- kol/src/main/java/com/kol/kol/controller/welcome.java
+++ kol/src/main/java/com/kol/kol/controller/welcome.java
@@ -1,16 +1,14 @@
 package com.kol.kol.controller;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 @ComponentScan
-@RestController
+@Controller
 
 public class welcome {
-    @GetMapping("hi")
-    public String helloworld(){
-        return "<h2>Heello</h2>";
+    @GetMapping("api")
+    public String getLoginpage(){
+        return "login";
     }
 }
